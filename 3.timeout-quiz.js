fe123e12672c019b3c4be0ed4a1a9f50b23c4cb9
Kwon Ena <eeena98@naver.com>
2022-06-07 23:47:10 +0900

    Study : timeout 구현

---
+++ 3.timeout-quiz.js
@@ -0,0 +1,15 @@
+function runInDelay(callback, seconds) {
+  if (!callback) {
+    throw new Error("callback 함수를 전달 해야 함");
+  }
+  if (!seconds || seconds < 0) {
+    throw new Error("seconds는 0보다 커야 함");
+  }
+  setTimeout(callback, seconds * 1000);
+}
+
+try {
+  runInDelay(() => {
+    console.log("타이머 완료!");
+  }, 2);
+} catch (error) {}
