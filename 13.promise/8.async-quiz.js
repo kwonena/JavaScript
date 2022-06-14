9048f617e346f1b0fd81654f71f5151ce60794f3
Kwon Ena <eeena98@naver.com>
2022-06-14 23:32:57 +0900

    Study : async quiz

---
+++ 13.promise/8.async-quiz.js
@@ -0,0 +1,26 @@
+function fetchEgg(chicken) {
+  return Promise.resolve(`${chicken} => 🥚`);
+}
+
+function fryEgg(egg) {
+  return Promise.resolve(`${egg} => 🍳`);
+}
+
+function getChicken() {
+  return Promise.reject(new Error("치킨을 가지고 올 수 없음"));
+  // return Promise.resolve("🪴 => 🐔");
+}
+
+// 전달 인자와 출력 인자가 같으면 생략 가능
+async function makeFriedEgg() {
+  let chicken;
+  try {
+    chicken = await getChicken();
+  } catch {
+    chicken = "🐔";
+  }
+  const egg = await fetchEgg(chicken);
+  return fryEgg(egg);
+}
+
+makeFriedEgg().then(console.log);
